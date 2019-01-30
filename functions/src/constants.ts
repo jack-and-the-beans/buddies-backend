ec281ea41f9d3a705410fd0e3a450c679aa6e340
Noah Allen <noahtallen@gmail.com>
2019-01-30 15:51:45 -0500

    Define test values for algolia config
    We use these in case the firebase environment is not defined. They're garbage,
    so Algolia won't connect to anything if we accidentally initialize it with them.

--- functions/src/constants.ts
+++ functions/src/constants.ts
@@ -3,8 +3,7 @@ import * as functions from 'firebase-functions'
 export const USER_INDEX_NAME = 'BUD_USERS'
 export const ACTIVITY_INDEX_NAME = 'BUD_ACTIVITIES'
 
-const { app_id, api_key, search_api_key } = functions.config().algolia
-export const ALGOLIA_APP_ID = app_id
-export const ALGOLIA_ADMIN_API_KEY = api_key
-export const ALGOLIA_SEARCH_API_KEY = search_api_key
+export const ALGOLIA_APP_ID = functions.config().algolia ? functions.config().algolia.app_id : 'NOPE'
+export const ALGOLIA_ADMIN_API_KEY = functions.config().algolia ? functions.config().algolia.api_key : 'NOPE'
+export const ALGOLIA_SEARCH_API_KEY = functions.config().algolia ? functions.config().algolia.search_api_key : 'NOPE'
 export const USER_SEARCH_RADIUS = 30000 // 30 km
