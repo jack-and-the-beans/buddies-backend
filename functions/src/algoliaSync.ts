2e4126d6de8e6e21a87242d4edd1c70a0c1943f0
Noah Allen <noahtallen@gmail.com>
2019-01-30 13:34:10 -0500

    Create tests for Algolia Sync

--- functions/src/algoliaSync.ts
+++ functions/src/algoliaSync.ts
@@ -7,22 +7,21 @@ const client = algoliasearch(constants.ALGOLIA_APP_ID, constants.ALGOLIA_ADMIN_A
 
 // Triggered on create, update, and delete:
 export const sendActivityDataToAlgolia = functions.firestore.document('activities/{activity_id}').onWrite((change, context) => {
-  return algoliaSync<AlgoliaActivity>(constants.ACTIVITY_INDEX_NAME, change, getAlgoliaActivityData, hasActivityChanged)
+  return algoliaSync<AlgoliaActivity>(client.initIndex(constants.ACTIVITY_INDEX_NAME), change, getAlgoliaActivityData, hasActivityChanged)
 })
 
 // Triggered on create, update, and delete:
 export const sendUserDataToAlgolia = functions.firestore.document('activities/{activity_id}').onWrite((change, context) => {
-  return algoliaSync<AlgoliaUser>(constants.USER_INDEX_NAME, change, getAlgoliaUserData, hasUserChanged)
+  return algoliaSync<AlgoliaUser>(client.initIndex(constants.USER_INDEX_NAME), change, getAlgoliaUserData, hasUserChanged)
 })
 
 // Creates, updates, or deletes objects in the given Algolia index based on the
 // data from the document change. Utilizes dataFunc and compareFunc respectively
 // to get database data from change and to compare entries of type T.
-async function algoliaSync<T extends AlgoliaBase>(index: string,
+export async function algoliaSync<T extends AlgoliaBase>(algoliaIndex: algoliasearch.Index,
                               change: functions.Change<FirebaseFirestore.DocumentSnapshot>,
                               dataFunc: (doc: FirebaseFirestore.DocumentSnapshot) => T | null,
                               compareFunc: (a: T, b: T) => boolean): Promise<void> {
-  const algoliaIndex = client.initIndex(index)
   const newData = dataFunc(change.after)
   const oldData = dataFunc(change.before);
   
@@ -42,7 +41,7 @@ async function algoliaSync<T extends AlgoliaBase>(index: string,
   }
 }
 
-function getAlgoliaUserData(doc: FirebaseFirestore.DocumentSnapshot): AlgoliaUser | null {
+export function getAlgoliaUserData(doc: FirebaseFirestore.DocumentSnapshot): AlgoliaUser | null {
   const data = doc.data()
   if (!doc.exists || !data) {
     return null
@@ -56,13 +55,13 @@ function getAlgoliaUserData(doc: FirebaseFirestore.DocumentSnapshot): AlgoliaUse
     block_filter,
     objectID: doc.id,
     favorite_topics: data.favorite_topics,
-    should_send_activity_suggestion_notification: data.should_send_activity_suggestion_notification,
+    should_send_activity_suggestion_notification: data.should_send_activity_suggestion_notification == null ? true : data.should_send_activity_suggestion_notification,
     _geoloc: locationConvert(data.location),
     notification_token: data.notification_token ? data.notification_token : ''
   }
 }
 
-function hasUserChanged(a: AlgoliaUser, b: AlgoliaUser): boolean {
+export function hasUserChanged(a: AlgoliaUser, b: AlgoliaUser): boolean {
   if (a.should_send_activity_suggestion_notification !== b.should_send_activity_suggestion_notification ||
       a._geoloc.lng !== b._geoloc.lng ||
       a._geoloc.lat !== b._geoloc.lat ||
@@ -76,7 +75,7 @@ function hasUserChanged(a: AlgoliaUser, b: AlgoliaUser): boolean {
 }
 
 // Gets the data we need from the document:
-function getAlgoliaActivityData(doc: FirebaseFirestore.DocumentSnapshot): AlgoliaActivity | null {
+export function getAlgoliaActivityData(doc: FirebaseFirestore.DocumentSnapshot): AlgoliaActivity | null {
   const data = doc.data()
   if (!doc.exists || !data) {
     return null
@@ -92,7 +91,7 @@ function getAlgoliaActivityData(doc: FirebaseFirestore.DocumentSnapshot): Algoli
   }
 }
 
-function hasActivityChanged(a: AlgoliaActivity, b: AlgoliaActivity): boolean {
+export function hasActivityChanged(a: AlgoliaActivity, b: AlgoliaActivity): boolean {
   if (a.title !== b.title ||
       a.description !== b.description ||
       a._geoloc.lat !== b._geoloc.lat ||
@@ -106,7 +105,7 @@ function hasActivityChanged(a: AlgoliaActivity, b: AlgoliaActivity): boolean {
   return false
 }
 
-function locationConvert(loc: FirebaseFirestore.GeoPoint): AlgoliaGeoPoint {
+export function locationConvert(loc: FirebaseFirestore.GeoPoint): AlgoliaGeoPoint {
   return {
     lat: loc.latitude,
     lng: loc.longitude,
@@ -114,6 +113,6 @@ function locationConvert(loc: FirebaseFirestore.GeoPoint): AlgoliaGeoPoint {
 }
 
 // Deep equal check an array of strings for equality:
-function areArrsDifferent(a: string[], b: string[]): boolean {
+export function areArrsDifferent(a: string[], b: string[]): boolean {
   return a.sort().join(',') !== b.sort().join(',')
 }
