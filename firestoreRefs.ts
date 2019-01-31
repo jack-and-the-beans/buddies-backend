8ed35539ad37f08adefd3d2e4c89db1e2806e855
Noah Allen <noahtallen@gmail.com>
2019-01-31 13:12:45 -0500

    Send a notification to each user on message creation

--- firestoreRefs.ts
+++ firestoreRefs.ts
@@ -6,5 +6,6 @@ export default function refs(firestore: FirebaseFirestore.Firestore) {
         activity: (activityId: string) => firestore.collection('activities').doc(activityId),
         chat: (activityId: string) => firestore.collection('activities').doc(activityId).collection('chat'),
         users: () => firestore.collection('users'),
+        user: (userId: string) => firestore.collection('users').doc(userId)
     }
 }
