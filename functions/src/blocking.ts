a49e0dfc71c3fdb1ce2545307091d1641afc6c22
Noah Allen <noahtallen@gmail.com>
2019-04-05 19:01:29 -0400

    Create some crazy mocks for tests

--- functions/src/blocking.ts
+++ functions/src/blocking.ts
@@ -1,108 +1,106 @@
-import * as functions from 'firebase-functions'
+import { EventContext } from 'firebase-functions'
+import { CallableContext } from 'firebase-functions/lib/providers/https'
 import * as admin from 'firebase-admin'
 import Ref from './firestoreRefs'
 import { get, includes } from 'lodash'
 
-// import refs from '../../firestoreRefs.js'
-try { admin.initializeApp() } catch (e) {}
+export default class Blocking {
+    constructor(public db: FirebaseFirestore.Firestore) { }
 
-const db = admin.firestore();
+    block = async (type: 'user' | 'activity', blocker_id: string, blocked_id: string) => {
 
-export async function block(type: 'user' | 'activity', blocker_id: string, blocked_id: string): Promise<void> {
-
-    //use a batch to atomically update both users, works offline
-    const batch = db.batch();
-
-    const refs = Ref(db);
-
-    if(type === "user"){
-        const blockerRef = refs.account(blocker_id);
-        const blockedRef = refs.account(blocked_id);
-
-        batch.update(blockedRef, {
-            blocked_by: admin.firestore.FieldValue.arrayUnion(blocker_id)
-        });
-
-        batch.update(blockerRef, {
-            blocked_users: admin.firestore.FieldValue.arrayUnion(blocked_id)
-        });
-    } else if (type === "activity"){
-        const blockerRef = refs.account(blocker_id);
-        batch.update(blockerRef, {
-            blocked_activities: admin.firestore.FieldValue.arrayUnion(blocked_id)
-        });
+        //use a batch to atomically update both users, works offline
+        const batch = this.db.batch();
+    
+        const refs = Ref(this.db);
+    
+        if(type === "user"){
+            const blockerRef = refs.account(blocker_id);
+            const blockedRef = refs.account(blocked_id);
+    
+            batch.update(blockedRef, {
+                blocked_by: admin.firestore.FieldValue.arrayUnion(blocker_id)
+            });
+    
+            batch.update(blockerRef, {
+                blocked_users: admin.firestore.FieldValue.arrayUnion(blocked_id)
+            });
+        } else if (type === "activity"){
+            const blockerRef = refs.account(blocker_id);
+            batch.update(blockerRef, {
+                blocked_activities: admin.firestore.FieldValue.arrayUnion(blocked_id)
+            });
+        }
+    
+        try {
+            await batch.commit();
+        } catch(e) {
+            console.error(`User ${blocker_id} failed blocking ${type} ${blocked_id}`);
+            console.error(e);
+            throw e;
+        }
     }
 
-    try {
-        await batch.commit();
-    } catch(e) {
-        console.error(`User ${blocker_id} failed blocking ${type} ${blocked_id}`);
-        console.error(e);
-        throw e;
+    blockUserOnCall = async (data: any, context: CallableContext) => {
+        await this.block('user', data.blocker_id, data.blocked_id);
     }
-}
 
-export const blockUserOnCall = functions.https.onCall(async (data, context) => {
-    await block('user', data.blocker_id, data.blocked_id);
-})
-
-export function getReportIds(actorField: string, targetField: string, snap: FirebaseFirestore.DocumentSnapshot){
-    const newReport = snap.data();
-    const actor_id = newReport ? newReport[actorField] : null;
-    const target_id = newReport ? newReport[targetField] : null;
-    return { actor_id, target_id };
-}
-
-export const newReportedUser = functions.firestore
-    .document('user_report/{reportId}').onCreate(async (snap, context) => {
-        const { actor_id, target_id } = getReportIds('report_by_id', 'reported_user_id', snap);
+    newReportedUser = async (snap: FirebaseFirestore.DocumentSnapshot, context: EventContext) => {
+        const { actor_id, target_id } = this.getReportIds('report_by_id', 'reported_user_id', snap);
         if(actor_id && target_id){
-            await block('user', actor_id, target_id);
-            await removeMyselfFromSharedActivities(actor_id, target_id)
+            await this.block('user', actor_id, target_id);
+            await this.removeMyselfFromSharedActivities(actor_id, target_id)
         } else {
             console.error(`Blocker "${actor_id}" or blocked "${target_id}" does not exist`);
         }
-    });
+    }
 
-export const newReportedActivity = functions.firestore
-    .document('activity_report/{reportId}').onCreate(async (snap, context) => {
-        const { actor_id, target_id } = getReportIds('report_by_id', 'reported_activity_id', snap);
+    newReportedActivity = async (snap: FirebaseFirestore.DocumentSnapshot, context: EventContext) => {
+        const { actor_id, target_id } = this.getReportIds('report_by_id', 'reported_activity_id', snap);
         if(actor_id && target_id){
-            await block('activity', actor_id, target_id);
-            const ref = Ref(db).activities().doc(target_id)
-            await updateActivityOnBlock(ref, actor_id)
+            await this.block('activity', actor_id, target_id);
+            const ref = Ref(this.db).activities().doc(target_id)
+            await this.updateActivityOnBlock(ref, actor_id)
         } else {
             console.error(`Blocker "${actor_id}" or blocked "${target_id}" does not exist`);
         }
-    });
+    }
 
-// We want to remove ME from evry activity I share with "blockedUser"
- async function removeMyselfFromSharedActivities(me: string, blockedUser: string): Promise<void> {
-     // Query for all activities which I am in. Note that we cannot
-     // chain array-contains to include multiple values
-    const query = Ref(db).activities().where('members', 'array-contains', me)
-    const results = await query.get()
+    getReportIds = (actorField: string, targetField: string, snap: FirebaseFirestore.DocumentSnapshot) => {
+        const newReport = snap.data();
+        const actor_id = newReport ? newReport[actorField] : null;
+        const target_id = newReport ? newReport[targetField] : null;
+        return { actor_id, target_id };
+    }
 
-    // Find all of my activities which include the blocked user
-    const matchingActivities = results.docs.filter( doc => {
-        const data = doc.data()
-        const members = get(data, 'members', []) as string[]
-        return includes(members, blockedUser)
-    })
-    
-    // Handle remove/ban on each shared activity:
-    const tasks = matchingActivities.map( ({ ref }) => updateActivityOnBlock(ref, me) )
-    
-    // Execute all update tasks in parallel
-    await Promise.all(tasks)
-}
+    // We want to remove ME from evry activity I share with "blockedUser"
+    removeMyselfFromSharedActivities = async (me: string, blockedUser: string) => {
+        // Query for all activities which I am in. Note that we cannot
+        // chain array-contains to include multiple values
+        const query = Ref(this.db).activities().where('members', 'array-contains', me)
+        const results = await query.get()
 
-// When we report/block an activity, or report/block a user in the activity, we want to:
-// 1) remove ourselves from the activity and 2) ban ourselves from the activity.
-async function updateActivityOnBlock(activityRef: FirebaseFirestore.DocumentReference, me: string): Promise<void> {
-    // Remove myself from members; add myself to ban list:
-    await activityRef.update({
-        members: admin.firestore.FieldValue.arrayRemove(me),
-        banned_users: admin.firestore.FieldValue.arrayUnion(me)
-    })
+        // Find all of my activities which include the blocked user
+        const matchingActivities = results.docs.filter( doc => {
+            const data = doc.data()
+            const members = get(data, 'members', []) as string[]
+            return includes(members, blockedUser)
+        })
+        
+        // Handle remove/ban on each shared activity:
+        const tasks = matchingActivities.map( ({ ref }) => this.updateActivityOnBlock(ref, me) )
+        
+        // Execute all update tasks in parallel
+        await Promise.all(tasks)
+    }
+
+    // When we report/block an activity, or report/block a user in the activity, we want to:
+    // 1) remove ourselves from the activity and 2) ban ourselves from the activity.
+    updateActivityOnBlock = async (activityRef: FirebaseFirestore.DocumentReference, me: string) => {
+        // Remove myself from members; add myself to ban list:
+        await activityRef.update({
+            members: admin.firestore.FieldValue.arrayRemove(me),
+            banned_users: admin.firestore.FieldValue.arrayUnion(me)
+        })
+    }
 }
